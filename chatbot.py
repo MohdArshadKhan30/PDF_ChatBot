860af5cb0b5db2e8736d9861aeb71a7c55cc7dc3
MohdArshadKhan30 <khanarshad3033a@gmail.com>
2026-05-17 22:49:18 +0530

    Implement PDF upload and text extraction features
    Added functionality to handle PDF uploads and extract text for chatbot interaction.

--- chatbot.py
+++ chatbot.py
@@ -20,7 +20,7 @@ st.header("📚 Chat with your PDF ")
 # -------------------- LOAD ENV --------------------
 load_dotenv()
 
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+GROQ_API_KEY = ${{secrets.GROQ_API_KEY}}
 
 if not GROQ_API_KEY:
     st.error("❌ GROQ_API_KEY not found in .env")
